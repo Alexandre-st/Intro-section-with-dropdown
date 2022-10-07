64cf385197ab8438dc8fca0885a005f5c6c09532
Alexandre-st <alexandre.saintprix@gmail.com>
2022-10-07 12:45:45 +0200

    Add a lot of things but need to fix the navbar with the drodown -WIP-

--- src/app.js
+++ src/app.js
@@ -1,7 +1,37 @@
+// Selector for the menu
 const menu = document.querySelector('.menu');
 const showMenu = document.querySelector('.nav-wrapper');
+// Selector for the rotation
+// const marker = document.querySelector('.marker');
+// const details = document.querySelector('.nav-list-details');
+const details = document.querySelectorAll('.nav-list-details');
 
+// Logic for the menu on mobile devices
 menu.addEventListener('click', () => {
     menu.classList.toggle('active'); 
     showMenu.classList.toggle('active');
 });
+
+// Logic to rotate the marker when the details is clicked
+details.forEach((detail) => {
+    detail.addEventListener('toggle', () => {
+            const marker = document.querySelector('.marker');
+            // marker.style.transform = detail.open ? 'rotate(180deg)' : 'rotate(0)';
+        if (detail.open) {
+            marker.style.transform = 'rotate(180deg)';
+        } else {
+            marker.style.transform = 'rotate(0)';
+        }
+        // markers.forEach(marker => {
+        //     if (detail.open) {
+        //         marker.style.transform = 'rotate(180deg)';
+        //     } else {
+        //         marker.style.transform = 'rotate(0)';
+        //     }
+        // })
+    })
+});
+
+// details.forEach(detail => console.log(detail));
+
+// console.log(details.open);
