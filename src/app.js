fa242ce8b6d7673631c8f115ac9833dc642eabb1
Alexandre-st <alexandre.saintprix@gmail.com>
2022-10-12 10:15:01 +0200

    Menu on mobile working, and have animations, now need some effects on the project

--- src/app.js
+++ src/app.js
@@ -4,14 +4,18 @@ const showMenu = document.querySelector('.nav-wrapper');
 // Selector for the dropdown rotation
 const firstDetail = document.querySelector('.nav-list-details-first');
 const secondDetail = document.querySelector('.nav-list-details-second');
+const menuDetail = document.querySelector('.menu-details-first');
+const secondMenuDetail = document.querySelector('.menu-details-second');
 const marker = document.querySelector('.marker');
 const marker2 = document.querySelector('.marker2');
+const menuMarker = document.querySelector('.menu-marker')
+const menuMarker2 = document.querySelector('.menu-marker2')
 
 // Logic for the menu on mobile devices
-menu.onmouseover = () => {
+menu.addEventListener('click', () => {
     menu.classList.toggle('active'); 
     showMenu.classList.toggle('active');
-};
+});
 
 // Effect for the toggle dropdown menu
 firstDetail.addEventListener('toggle', () => {
@@ -20,5 +24,14 @@ firstDetail.addEventListener('toggle', () => {
 
 secondDetail.addEventListener('toggle', () => {
     secondDetail.open ? marker2.style.transform = 'rotate(180deg)' : marker2.style.transform = 'rotate(0)';
-})
+});
+
+// Effect for the mobile menu 
+menuDetail.addEventListener('toggle', () => {
+    menuDetail.open ? menuMarker.style.transform = 'rotate(180deg)' : menuMarker.style.transform = 'rotate(0)';
+});
+
+secondMenuDetail.addEventListener('toggle', () => {
+    secondMenuDetail.open ? menuMarker2.style.transform = 'rotate(180deg)' : menuMarker2.style.transform = 'rotate(0)';
+});
 
